429feeb3b7d245e7a23125ffd29f535a299b68a2
vivek-kancharla <vivekkancharla3098@gmail.com>
2022-01-12 21:13:09 +0530

    Noargument

---
+++ Noargument/src/Noargument.java
@@ -0,0 +1,14 @@
+//programm to implement No-Argument Constructor
+public class Noargument {
+	//Declaring Constructor with no argument
+	public Noargument()
+	
+	{
+		System.out.println(".......No argument constructor......");
+	}
+	public static void main(String[] args) {
+		//Creating object of class type
+		Noargument noarg=new Noargument();
+	}
+
+}
