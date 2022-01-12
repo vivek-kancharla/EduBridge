ff18ecab394ae67f880aa585d07b812875a221fe
vivek-kancharla <vivekkancharla3098@gmail.com>
2022-01-12 23:14:39 +0530

    addition

---
+++ threenumbers/src/threenumbers/threenumbers.java
@@ -0,0 +1,16 @@
+package threenumbers;
+
+public class threenumbers {
+	public static int add(int a,int b,int c)
+	{
+		return a+b+c;
+	}
+	public static void main(String[] args) {
+		
+		int result;
+		result=add(5,9,4);
+		System.out.println("Summation of three numbers is "+result);
+		
+	}
+
+}
