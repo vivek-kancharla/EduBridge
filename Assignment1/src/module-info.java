8abc550fd326b63403abab1dd9a065f739fccb90
vivek-kancharla <vivekkancharla3098@gmail.com>
2022-01-12 20:30:49 +0530

    welcometojava

---
+++ Assignment1/src/module-info.java
@@ -0,0 +1,12 @@
+module assignment1 {
+	public class assignment {
+		public static void string()
+		{
+			System.out.println("welcome to java prog");
+		}
+		public static void main(String[] args)
+		{
+			string();
+		}
+	}
+
