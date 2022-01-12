03f44eb2b753cdef285f05300c87de913f3ecd49
vivek-kancharla <vivekkancharla3098@gmail.com>
2022-01-12 21:19:24 +0530

    hellopro

---
+++ hellopro/src/hellopro/mainclass.java
@@ -0,0 +1,13 @@
+package hellopro;
+
+public class mainclass {
+
+	public static void main(String[]args) {
+		String ch="Hello";
+		String str="Welcome";
+		System.out.println(ch);
+		System.out.println(ch+" "+ch);
+		System.out.print(str+" "+str);
+		
+	}
+}
